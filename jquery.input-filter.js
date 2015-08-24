143cac57edce82fdf406562035a54eadfd107f83
Elegant Lai <elegant.lai@kiwifi.cn>
2015-08-24 21:22:45 +0800

    fixed bug
    如果是过滤，长度限制失效。

--- jquery.input-filter.js
+++ jquery.input-filter.js
@@ -1,120 +1,124 @@
 ;
 if (typeof jQuery === 'undefined') {
-    throw new Error("InputFilter's JavaScript requires jQuery")
+	throw new Error("InputFilter's JavaScript requires jQuery")
 }
 (function($, win, doc, undefined) {
-    'use strict';
+	'use strict';
 
-    var InputFilter = function(element, options) {
-        this.options = options;
-        this.element = element;
-    };
-    InputFilter.prototype = {
-        digitFilter: function() {
-            var value = this.element.value,
-                options = this.options;
-            if (value === undefined) {
-                value = "";
-            } else {
-                value = value.replace(/[^\d]+/g, "");
-                if (options.min !== null && value < options.min) {
-                    value = options.min;
-                }
-                if (options.max !== null && value > options.max) {
-                    value = options.max;
-                }
-            }
-            return value;
-        },
-        alphaFilter: function() {
-            var value = this.element.value,
-                options = this.options;
-            if (value === undefined) {
-                value = "";
-            } else {
-                var reg;
-                if (options.uppercase && options.lowercase) {
-                    reg = /[^A-Za-z]+/g;
-                } else if (options.uppercase && !options.lowercase) {
-                    reg = /[^A-Z]+/g;
-                } else {
-                    reg = /[^a-z]+/g;
-                }
-                value = value.replace(reg, "");
-                if (options.transform === "uppercase") {
-                    value = value.toUpperCase();
-                } else if (options.transform === "lowercase") {
-                    value = value.toLocaleLowerCase();
-                }
-            }
-            return value;
-        },
-        alnumFilter: function() {
-            var value = this.element.value,
-                options = this.options;
-            if (value === undefined) {
-                value = "";
-            } else {
-                var reg;
-                if (options.uppercase && options.lowercase) {
-                    reg = /[^A-Za-z0-9]+/g;
-                } else if (options.uppercase && !options.lowercase) {
-                    reg = /[^A-Z0-9]+/g;
-                } else {
-                    reg = /[^a-z0-9]+/g;
-                }
-                value = value.replace(reg, "");
-                if (options.transform === "uppercase") {
-                    value = value.toUpperCase();
-                } else if (options.transform === "lowercase") {
-                    value = value.toLocaleLowerCase();
-                }
-            }
-            return value;
-        }
-    };
-    $.fn.inputFilter = function(options) {
-        options = $.extend({}, $.fn.inputFilter.options, options || {});
-        var elements = this;
-        return elements.each(function() {
-            var element = this,
-                valueChange = options.valueChange,
-                inputFilter = new InputFilter(element, options),
-                eventHandler = function(event) {
-                    var filterValue=inputFilter[options.type + "Filter"]();
-                    console.log("filterValue: "+filterValue);
-                    if (typeof options.length === "number") {
-                        filterValue=filterValue.slice(0, options.length);
-                    }
-                    timeout=setTimeout(function(){
-                        element.value=filterValue;
-                    },100);
-                    console.log("value: "+element.value);
-                    valueChange(element, element.value);
-                };
-            try {
-                if (element.addEventListener) {
-                    element.addEventListener("input", eventHandler, false);
-                } else {
-                    element.attachEvent("onpropertychange", function() {
-                        return eventHandler.call(element);
-                    });
-                }
-            } catch (e) {
-                throw new Error(e.name + ": " + e.message);
-            }
-            return this;
-        });
-    };
-    $.fn.inputFilter.options = {
-        "type": "alnum",
-        "length": null,
-        "min": 0,
-        "max": Infinity,
-        "uppercase": true,
-        "lowercase": true,
-        "transform": null,
-        "valueChange": function(element, value) {}
+	var InputFilter = function(element, options) {
+		this.options = options;
+		this.element = element;
+	};
+	InputFilter.prototype = {
+		digitFilter: function() {
+			var value = this.element.value,
+				options = this.options;
+			if (value === undefined) {
+				value = " ";
+			} else {
+				if (!/^[1-9]\d*$/.test(value) || value > options.max) {
+					console.info(!/^[1-9]\d*$/.test(value) || value > options.max);
+					//如果输出不符合预期，则进行过滤
+					value = value.replace(/[^\d]+/g, "");
+					if (options.min !== null && value < options.min) {
+						value = options.min;
+					}
+					if (options.max !== null && value > options.max) {
+						value = options.max;
+					}
+				}
+			}
+			return value;
+		},
+		alphaFilter: function() {
+			var value = this.element.value,
+				options = this.options;
+			if (value === undefined) {
+				value = "";
+			} else {
+				var reg;
+				if (options.uppercase && options.lowercase) {
+					reg = /[^A-Za-z]+/g;
+				} else if (options.uppercase && !options.lowercase) {
+					reg = /[^A-Z]+/g;
+				} else {
+					reg = /[^a-z]+/g;
+				}
+				value = value.replace(reg, "");
+				if (options.transform === "uppercase") {
+					value = value.toUpperCase();
+				} else if (options.transform === "lowercase") {
+					value = value.toLocaleLowerCase();
+				}
+			}
+			return value;
+		},
+		alnumFilter: function() {
+			var value = this.element.value,
+				options = this.options;
+			if (value === undefined) {
+				value = "";
+			} else {
+				var reg;
+				if (options.uppercase && options.lowercase) {
+					reg = /[^A-Za-z0-9]+/g;
+				} else if (options.uppercase && !options.lowercase) {
+					reg = /[^A-Z0-9]+/g;
+				} else {
+					reg = /[^a-z0-9]+/g;
+				}
+				value = value.replace(reg, "");
+				if (options.transform === "uppercase") {
+					value = value.toUpperCase();
+				} else if (options.transform === "lowercase") {
+					value = value.toLocaleLowerCase();
+				}
+			}
+			return value;
+		}
+	};
 
-    };
+	$.fn.inputFilter = function(options) {
+		options = $.extend({}, $.fn.inputFilter.options, options || {});
+		var elements = this;
+		return elements.each(function() {
+			var element = this,
+				valueChange = options.valueChange,
+				inputFilter = new InputFilter(element, options),
+				eventHandler = function(event) {
+					var filterValue = inputFilter[options.type + "Filter"]();
+					if (typeof options.length === "number" && options.type !== "digit") {
+						filterValue = filterValue.slice(0, options.length);
+					}
+					var timeout = setTimeout(function() {
+						element.value = filterValue;
+					}, 100);
+					//console.log("value: " + element.value);
+					valueChange(element, element.value);
+				};
+			try {
+				if (element.addEventListener) {
+					element.addEventListener("input", eventHandler, false);
+				} else {
+					element.attachEvent("onpropertychange", function(event) {
+						return eventHandler.call(element, event);
+					});
+				}
+			} catch (e) {
+				throw new Error(e.name + ": " + e.message);
+			}
+			return this;
+		});
+	};
+	$.fn.inputFilter.options = {
+		"type": "alnum",
+		"length": null,
+		"min": 0,
+		"max": Infinity,
+		"uppercase": true,
+		"lowercase": true,
+		"transform": null,
+		"valueChange": function(element, value) {}
+
+	};
 })(jQuery, window, document);
